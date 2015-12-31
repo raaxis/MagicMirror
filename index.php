d7c56848af36b453639f11461015a1261f7ba054
Kevin <me@raaxis.net>
2015-12-31 19:46:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -47,6 +47,7 @@
 <script src="js/weather/weather.js" type="text/javascript"></script>
 <script src="js/time/time.js" type="text/javascript"></script>
 <script src="js/news/news.js" type="text/javascript"></script>
+<script src="js/news/news2.js" type="text/javascript"></script>
 <script src="js/main.js?nocache=<?php echo md5(microtime()) ?>"></script>
 <script src="js/saver.js"></script>
 <!-- <script src="js/socket.io.min.js"></script> -->
