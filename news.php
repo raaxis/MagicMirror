77b810399b2b42c2a318e8f0ef70aaa7922cd8b9
Kevin <me@raaxis.net>
2016-01-11 00:25:00 +0100

    Create news.php

---
+++ news.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+	<title>Magic Mirror</title>
+	<style type="text/css">
+		<?php include('css/main.css') ?>
+	</style>
+	<link rel="stylesheet" type="text/css" href="css/weather-icons.css">
+	<script type="text/javascript">
+		var gitHash = '<?php echo trim(`git rev-parse HEAD`) ?>';
+	</script>
+	<meta name="google" value="notranslate" />
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+</head>
+<body>
+
+	<div class="top center-fromtop200-hor">
+		<div class="news medium"></div>
+	</div>
+	
+	<div class="top center-fromtop100-hor">
+		<div class="news2 small"></div>
+	</div>
+
+
+<script src="js/jquery.js"></script>
+<script src="js/jquery.feedToJSON.js"></script>
+<script src="js/ical_parser.js"></script>
+<script src="js/moment-with-locales.min.js"></script>
+<script src="js/config.js"></script>
+<script src="js/rrule.js"></script>
+<script src="js/version/version.js" type="text/javascript"></script>
+<script src="js/news/news.js" type="text/javascript"></script>
+<script src="js/news/news2.js" type="text/javascript"></script>
+<script src="js/main.js?nocache=<?php echo md5(microtime()) ?>"></script>
+<!-- <script src="js/socket.io.min.js"></script> -->
+
+</body>
+</html>
